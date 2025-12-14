42c05820116760478839fa14ced5c5288c3d5077
[dyad] <Mishrahiranmaya747@gmail.com>
2025-12-15 02:03:40 +0530

    [dyad] Fix default import causing runtime error - wrote 3 file(s)

--- src/components/InstallAppPrompt.tsx
+++ src/components/InstallAppPrompt.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import Button from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 
 type BeforeInstallPromptEvent = Event & {
   prompt: () => Promise<void>;
--- src/components/ServiceWorkerUpdatePrompt.tsx
+++ src/components/ServiceWorkerUpdatePrompt.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import Button from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 
 export default function ServiceWorkerUpdatePrompt() {
   const [updateAvailable, setUpdateAvailable] = useState(false);
