fd2f7fa9b2b52a0be0349c0a955643956c9bda28
[dyad] <Mishrahiranmaya747@gmail.com>
2025-12-15 02:17:02 +0530

    [dyad] Check robots config to further optimize auth pages - wrote 1 file(s)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,10 +4,18 @@ import { getBaseUrl } from '@/utils/Helpers';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/sign-in',
+          '/sign-up',
+          '/fr/sign-in',
+          '/fr/sign-up',
+        ],
+      },
+    ],
     sitemap: `${getBaseUrl()}/sitemap.xml`,
   };
 }
